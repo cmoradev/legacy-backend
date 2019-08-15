ad1873831f342655c5fd5b498240d4ebfa56193d
Steeve Luis Angel Torres Agustin <iam@steeve.me>
2019-08-15 08:11:01 -0500

    Fix Route folder absolute

--- src/mini-store/mini-store-invoices/entities/mini-store-invoice.entity.ts
+++ src/mini-store/mini-store-invoices/entities/mini-store-invoice.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn} from 'typeorm';
 import {MiniStoreSalePayment} from '../../mini-store-sales-payments/entities/mini-store-sale-payment.entity';
 import { MiniStoreSale } from '../../mini-store-sales/entities/mini-store-sale.entity';
-import { User } from 'src/school-colegio-ingles/users/entities/user.entity';
+import { User } from '../../../school-colegio-ingles/users/entities/user.entity';
 
 @Entity('tie_facturas')
 export class MiniStoreInvoice {
