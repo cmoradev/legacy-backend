dd9bc45ab7b018ba0e91c6032bfefe83fedb01d0
Steeve Luis Angel Torres Agustin <iam@steeve.me>
2019-08-17 12:55:54 -0500

    Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const options = new DocumentBuilder()
+      .setTitle('Apps')
+      .setDescription('Es la aplicación de escuela')
+      .setVersion('1.0')
+      .addTag('School')
+      .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
