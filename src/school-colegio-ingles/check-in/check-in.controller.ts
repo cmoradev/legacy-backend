9b4b2608c67aa1e5c7a21787ef22fd4cf6db74cb
Steeve Luis Angel Torres Agustin <iam@steeve.me>
2019-09-21 09:45:46 -0500

    Cambios en las estructuras de stock y cantidades en las entidades Productos y Detalle de venta

--- src/school-colegio-ingles/check-in/check-in.controller.ts
+++ src/school-colegio-ingles/check-in/check-in.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { Crud, CrudController } from '@nestjsx/crud';
 import { CheckIn } from './entities/check-in.entity';
 import { CheckInService } from './check-in.service';
 import { base64ToImage } from '../../common/base64toImage';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Crud({
     model: {
@@ -25,5 +27,21 @@ export class CheckInController implements CrudController<CheckIn> {
         const dateImage = new Date().toLocaleString().replace(/\s/g, '_').replace(/\//g, '-').replace(/:/g, '-');
         const result = base64ToImage(signature, '/var/tmp/', { type: 'png', fileName: `signature-${dateImage}` });
         return await this.service.updateSignatureCheckIn(idCheckIn, result.fileName);
+    }
+
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('image', {
+        storage: diskStorage({
+            destination: (req, file, cb) => {
+                cb(null, './signatures');
+            },
+            filename: (req, file, cb) => {
+                cb(null, `${Date.now()}_${file.originalname}.png`);
+            },
+        }),
+    }))
+    async uploadFile(@UploadedFile() file, @Body('idCheckIn') idCheckIn: number) {
+        return await this.service.updateSignatureCheckIn(idCheckIn, file.originalname);
+
     }
 }
