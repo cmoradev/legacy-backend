922bf0daf9ae0067d49614eb2b3610d03aecc4be
Steeve Luis Angel Torres Agustin <iam@steeve.me>
2019-10-22 14:56:03 -0500

    Favicon Route Add

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
-import * as passport from 'passport';
 import { AuthModule } from './school-colegio-ingles/auth/auth.module';
+import * as path from 'path';
+import * as favicon from 'serve-favicon';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(favicon(path.join(__dirname, '..' , 'public', 'favicon.ico')))
   const options = new DocumentBuilder()
       .setTitle('Apps')
       .setDescription('Es la aplicación de escuela')
