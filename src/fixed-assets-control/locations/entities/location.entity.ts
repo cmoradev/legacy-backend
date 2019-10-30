cd6040eecdedf71c6aaf48c0a2a5d0fc8c703ac4
Cresencio Vázquez Flores <cresenciofl@icloud.com>
2019-10-30 13:22:25 -0500

    Se creo la migracion para vincular ubicaciones con los activos fijos que hayan sido asignados

--- src/fixed-assets-control/locations/entities/location.entity.ts
+++ src/fixed-assets-control/locations/entities/location.entity.ts
@@ -1,6 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from 'typeorm';
 import { Department } from '../../../school-colegio-ingles/departments/entities/department.entity';
 import { FixedAssetAssignment } from '../../fixed-assets-assignments/entities/fixed-asset-assignment.entity';
+import { FixedAsset } from '../../fixed-assets/entities/fixed-asset.entity';
 
 @Entity()
 export class Location {
@@ -19,6 +20,10 @@ export class Location {
     @OneToMany(type => FixedAssetAssignment,
         fixedAssetAssignmet => fixedAssetAssignmet.location)
     fixedAssetAssignments: FixedAssetAssignment[];
+
+    @OneToMany(type => FixedAsset,
+        fixedAsset => fixedAsset.location)
+    fixedAssets: FixedAsset[];
 
     @Column('timestamp', {
         nullable: false,
