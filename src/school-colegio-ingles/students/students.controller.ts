833a30a8f03c40930e9e3ce829e8550fc65187d9
Cresencio Vázquez Flores <cresenciofl@icloud.com>
2020-01-08 11:04:45 -0500

    Se creo recurso para obtener el total de alumnos por tipo

--- src/school-colegio-ingles/students/students.controller.ts
+++ src/school-colegio-ingles/students/students.controller.ts
@@ -1,40 +1,46 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { Crud, CrudController } from '@nestjsx/crud';
 import { Student } from './entities/student.entity';
 import { StudentsService } from './students.service';
 
 @Crud({
-  model: {
-    type: Student,
-  },
-  query: {
-    limit: 200,
-    join: {
-      family: {},
-      academiesModality: {},
-      studentCampus: {},
-      incidents: {},
-      'incidents.teacher': {},
-      'incidents.classroom': {},
-      'incidents.incidentClassification': {},
-      StudentInscriptions: {},
-      'StudentInscriptions.classroom': {},
-      'StudentInscriptions.cycle': {},
-      'StudentInscriptions.campus': {},
-      'StudentInscriptions.level': {},
-      'StudentInscriptions.grade': {},
-      'StudentInscriptions.group': {},
+    model: {
+        type: Student,
     },
-  },
+    query: {
+        limit: 200,
+        join: {
+            family: {},
+            academiesModality: {},
+            studentCampus: {},
+            incidents: {},
+            'incidents.teacher': {},
+            'incidents.classroom': {},
+            'incidents.incidentClassification': {},
+            StudentInscriptions: {},
+            'StudentInscriptions.classroom': {},
+            'StudentInscriptions.cycle': {},
+            'StudentInscriptions.campus': {},
+            'StudentInscriptions.level': {},
+            'StudentInscriptions.grade': {},
+            'StudentInscriptions.group': {},
+        },
+    },
 })
 @Controller()
 export class StudentsController implements CrudController<Student> {
-  constructor(
-    readonly service: StudentsService,
-  ) {
-  }
+    constructor(
+        readonly service: StudentsService,
+    ) {
+    }
 
-  get base(): CrudController<Student> {
-    return this;
-  }
+    get base(): CrudController<Student> {
+        return this;
+    }
+
+    @Get('/count-by-type/:studentType')
+    public async countStudentsByType(@Param('studentType') studentType: string) {
+        const total: Student[] = await this.service.find({ where: [{ typeStudent: studentType }] });
+        return total.length || 0;
+    }
 }
