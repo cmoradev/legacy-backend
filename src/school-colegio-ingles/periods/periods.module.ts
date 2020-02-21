2835df2d3086a3607093bc60b4169f0bc1df0807
MisaelMa <amisael.amir.misael@gmail.com>
2020-02-21 11:21:34 -0500

    se agrego la columna isActive en la tabal periodos

--- src/school-colegio-ingles/periods/periods.module.ts
+++ src/school-colegio-ingles/periods/periods.module.ts
@@ -2,9 +2,7 @@ import { Module } from '@nestjs/common';
 import { PeriodsService } from './periods.service';
 import { PeriodsController } from './periods.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PaymentPlan } from '../payment-plans/entities/payment-plan.entity';
 import { Periods } from './entities/periods.entity';
-import { Connection } from 'typeorm';
 import { ColegioDBNameConnection } from '../../databases/colegiodb.service';
 
 @Module({
