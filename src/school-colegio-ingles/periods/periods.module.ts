efb8e49e56d8189bb46cb50f63348be92d596681
MisaelMa <amisael.amir.misael@gmail.com>
2020-02-21 08:49:41 -0500

    se agrego el crudnest para la api

--- src/school-colegio-ingles/periods/periods.module.ts
+++ src/school-colegio-ingles/periods/periods.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PeriodsService } from './periods.service';
 import { PeriodsController } from './periods.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PaymentPlan } from '../payment-plans/entities/payment-plan.entity';
+import { Periods } from './entities/periods.entity';
+import { Connection } from 'typeorm';
+import { ColegioDBNameConnection } from '../../databases/colegiodb.service';
 
 @Module({
-  providers: [PeriodsService],
-  controllers: [PeriodsController]
+    imports: [TypeOrmModule.forFeature([Periods], ColegioDBNameConnection)],
+    providers: [PeriodsService],
+    controllers: [PeriodsController],
 })
-export class PeriodsModule {}
+export class PeriodsModule {
+}
--- src/school-colegio-ingles/periods/periods.service.ts
+++ src/school-colegio-ingles/periods/periods.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Periods } from './entities/periods.entity';
+import { ColegioDBNameConnection } from '../../databases/colegiodb.service';
 
 @Injectable()
-export class PeriodsService {}
+export class PeriodsService extends TypeOrmCrudService<Periods> {
+    constructor(@InjectRepository(Periods, ColegioDBNameConnection)
+                private readonly paymentPlanRepository: Repository<Periods>) {
+        super(paymentPlanRepository);
+    }
+}
