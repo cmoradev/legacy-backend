f71934b2464ea1bc94d392fbccaa8de8060931c3
MisaelMa <amisael.amir.misael@gmail.com>
2020-02-21 10:26:23 -0500

    se agrego la columna de isActive en peridos

--- src/migrations/1582292326029-addPeriodsTable.ts
+++ src/migrations/1582292326029-addPeriodsTable.ts
@@ -4,7 +4,7 @@ export class addPeriodsTable1582292326029 implements MigrationInterface {
     name = 'addPeriodsTable1582292326029'
 
     public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query("CREATE TABLE `periods` (`id` int NOT NULL AUTO_INCREMENT, `createdAt` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updatedAt` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `version` int NOT NULL DEFAULT 0, `uuid` varchar(36) NOT NULL, `name` varchar(255) NOT NULL, `description` varchar(300) NULL, `startDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP, `endDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP, `periodsCycleId` int NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
+        await queryRunner.query("CREATE TABLE `periods` (`id` int NOT NULL AUTO_INCREMENT, `createdAt` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updatedAt` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `version` int NOT NULL DEFAULT 0, `uuid` varchar(36) NOT NULL, `name` varchar(255) NOT NULL, `description` varchar(300) NULL, `startDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP, `endDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP, `isActive` TINYINT(1) NOT NULL DEFAULT '1', `periodsCycleId` int NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
         await queryRunner.query("ALTER TABLE `periods` ADD CONSTRAINT `FK_2a82133d9079eaa4d87a0bad3ac` FOREIGN KEY (`periodsCycleId`) REFERENCES `ciclos`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION", undefined);
     }
 
