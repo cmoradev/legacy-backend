4c803615fda7761c985eedc5b973e079eaca9eb2
MisaelMa <amisael.amir.misael@gmail.com>
2020-02-24 11:43:23 -0500

    se agrego la migracion

---
+++ src/migrations/1582562083316-statusPayments.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class statusPayments1582562083316 implements MigrationInterface {
+    name = 'statusPayments1582562083316'
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query("ALTER TABLE `school_payment` ADD `conceptType` enum ('1', '2', '3', '4', '5') NOT NULL DEFAULT '1'", undefined);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query("ALTER TABLE `school_payment` DROP COLUMN `conceptType`", undefined);
+    }
+
+}
