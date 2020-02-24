5acad9abb0f0f27e05eeb8a7797e1c5c404b0d04
MisaelMa <amisael.amir.misael@gmail.com>
2020-02-24 11:34:05 -0500

    se agrego el enum de estado de pago

--- src/school-colegio-ingles/school-payments/entities/school-payment.entity.ts
+++ src/school-colegio-ingles/school-payments/entities/school-payment.entity.ts
@@ -3,107 +3,116 @@ import { Base } from '../../../common/orm/entities/base.entity';
 import { SchoolChargeDetails } from '../../charges-school/school-charges-details/entities/school-charge-details.entity';
 import { Inscription } from '../../inscriptions/entities/inscription.entity';
 import { PaymentPlanConcept } from '../../payment-plan-concepts/entities/payment-plan-concept.entity';
+import { PaymentPlanConceptTypeEnum } from '../../payment-plan-concepts/enums/payment-plan-concept-type.enum';
+import { StatusPayment } from '../enums/statusPayment';
 
 
 @Entity()
 export class SchoolPayment extends Base {
-  @Column('varchar', {
-    nullable: false,
-    length: 20,
-  })
-  productCode: string;
+    @Column('varchar', {
+        nullable: false,
+        length: 20,
+    })
+    productCode: string;
 
-  @Column('varchar', {
-    nullable: false,
-    length: 20,
-  })
-  unitCode: string;
+    @Column('varchar', {
+        nullable: false,
+        length: 20,
+    })
+    unitCode: string;
 
-  @Column('varchar', {
-    nullable: true,
-    length: 100,
-  })
-  unit: string | null;
+    @Column('varchar', {
+        nullable: true,
+        length: 100,
+    })
+    unit: string | null;
 
-  @Column('varchar', {
-    nullable: true,
-    length: 250,
-  })
-  description: string | null;
+    @Column('varchar', {
+        nullable: true,
+        length: 250,
+    })
+    description: string | null;
 
-  @Column( 'int', {
-    nullable: false,
-    default: 1,
-  })
-  quantity: number;
+    @Column('int', {
+        nullable: false,
+        default: 1,
+    })
+    quantity: number;
 
-  @Column('int', {
-    nullable: true,
-  })
-  payDay: number;
-  @Column('int', {
-    nullable: true,
-  })
-  payMonth: number;
-  @Column('int', {
-    nullable: true,
-  })
-  paidDay: number;
-  @Column('int', {
-    nullable: true,
-  })
-  paidMonth: number;
+    @Column('int', {
+        nullable: true,
+    })
+    payDay: number;
+    @Column('int', {
+        nullable: true,
+    })
+    payMonth: number;
+    @Column('int', {
+        nullable: true,
+    })
+    paidDay: number;
+    @Column('int', {
+        nullable: true,
+    })
+    paidMonth: number;
 
-  @Column('varchar', {
-    nullable: true,
-  })
-  satCode: string | null;
+    @Column('varchar', {
+        nullable: true,
+    })
+    satCode: string | null;
 
-  @Column('decimal', {
-    nullable: true,
-    default: () => '0.000000',
-    precision: 15,
-    scale: 6,
-  })
-  price: number | null;
+    @Column('decimal', {
+        nullable: true,
+        default: () => '0.000000',
+        precision: 15,
+        scale: 6,
+    })
+    price: number | null;
 
-  @Column('boolean', {
-    nullable: false,
-    default: true,
-  })
-  withIva: number;
+    @Column('boolean', {
+        nullable: false,
+        default: true,
+    })
+    withIva: number;
 
-  @Column('decimal', {
-    nullable: false,
-    default: () => '0.000000',
-    precision: 15,
-    scale: 6,
-  })
-  iva: string;
+    @Column('decimal', {
+        nullable: false,
+        default: () => '0.000000',
+        precision: 15,
+        scale: 6,
+    })
+    iva: string;
 
-  @Column('date', {
-    nullable: true,
-  })
-  paidDate: Date;
-  @Column('date', {
-    nullable: true,
-  })
-  payDate: Date;
+    @Column('date', {
+        nullable: true,
+    })
+    paidDate: Date;
+    @Column('date', {
+        nullable: true,
+    })
+    payDate: Date;
 
-  @Column('boolean', {
-    nullable: false,
-    default: true,
-  })
-  isActive: boolean;
+    @Column('boolean', {
+        nullable: false,
+        default: true,
+    })
+    isActive: boolean;
 
-  @OneToOne(() => SchoolChargeDetails, (schoolCharge) => schoolCharge.schoolPayment)
-  @JoinColumn()
-  schoolChargeDetail: SchoolChargeDetails;
+    @Column('enum', {
+        nullable: false,
+        default: StatusPayment.Debit,
+        enum: StatusPayment,
+    })
+    conceptType: StatusPayment;
 
-  @OneToOne(() => PaymentPlanConcept, (paymentPlanConcept) => paymentPlanConcept.schoolPayment)
-  @JoinColumn()
-  paymentPlanConcept: PaymentPlanConcept;
+    @OneToOne(() => SchoolChargeDetails, (schoolCharge) => schoolCharge.schoolPayment)
+    @JoinColumn()
+    schoolChargeDetail: SchoolChargeDetails;
 
-  @ManyToOne(() => Inscription, (inscription) => inscription.schoolPayments)
-  inscription: Inscription;
+    @OneToOne(() => PaymentPlanConcept, (paymentPlanConcept) => paymentPlanConcept.schoolPayment)
+    @JoinColumn()
+    paymentPlanConcept: PaymentPlanConcept;
+
+    @ManyToOne(() => Inscription, (inscription) => inscription.schoolPayments)
+    inscription: Inscription;
 }
---
+++ src/school-colegio-ingles/school-payments/enums/statusPayment.ts
@@ -0,0 +1,7 @@
+export enum StatusPayment {
+    Debit = 1, // Adeudo
+    PaiOut = 2, // Pagado
+    Condoned = 3, // Condonado/Perdonado
+    Cancelled = 4, // Cancelado
+    Abonar = 5, // Abonado
+}
