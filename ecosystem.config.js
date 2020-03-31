9b83103be90816ad1cdd53af81a51697594295eb
MisaelMa <amisael.amir.misael@gmail.com>
2020-03-31 09:38:18 -0500

    se agrego un clouster de tortilleria

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -49,6 +49,23 @@ module.exports = {
     env_production: {
       NODE_ENV: 'refaccionaria'
     },
+  },
+    {
+      name: 'tortilleria',
+      script: 'dist/main.js',
 
-  }],
+      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+      args: 'one two',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '4G',
+      env_dev: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'tortilleria'
+      },
+
+    }],
 };
