6d4fd6bee45b82621029691db0f68506f017b898
MisaelMa <amisael.amir.misael@gmail.com>
2020-04-18 13:20:08 -0500

    se agrego la instancia de desarrollo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,71 +1,72 @@
 module.exports = {
-  apps : [{
-    name: 'colegio-api-core',
-    script: 'dist/main.js',
+    apps: [
+        {
+            name: 'colegio-api-core',
+            script: 'dist/main.js',
 
-    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-    args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '4G',
-    env_dev: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  },
-  {
-    name: 'rancho-api-core',
-    script: 'dist/main.js',
+            // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+            args: 'one two',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '4G',
+            env_dev: {
+                NODE_ENV: 'development',
+            },
+            env_production: {
+                NODE_ENV: 'production',
+            },
+        },
+        {
+            name: 'rancho-api-core',
+            script: 'dist/main.js',
 
-    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-    args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '4G',
-    env_dev: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'rancho'
-    }
-  },
-  {
-    name: 'refaccionaria-chan-api-core',
-    script: 'dist/main.js',
+            // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+            args: 'one two',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '4G',
+            env_dev: {
+                NODE_ENV: 'development',
+            },
+            env_production: {
+                NODE_ENV: 'rancho',
+            },
+        },
+        {
+            name: 'refaccionaria-chan-api-core',
+            script: 'dist/main.js',
 
-    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-    args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '4G',
-    env_dev: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'refaccionaria'
-    },
-  },
-    {
-      name: 'tortilleria',
-      script: 'dist/main.js',
+            // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+            args: 'one two',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '4G',
+            env_dev: {
+                NODE_ENV: 'development',
+            },
+            env_production: {
+                NODE_ENV: 'refaccionaria',
+            },
+        },
+        {
+            name: 'development',
+            script: 'dist/main.js',
 
-      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-      args: 'one two',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '4G',
-      env_dev: {
-        NODE_ENV: 'development'
-      },
-      env_production: {
-        NODE_ENV: 'tortilleria'
-      },
+            // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+            args: 'one two',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '4G',
+            env_dev: {
+                NODE_ENV: 'development',
+            },
+            env_production: {
+                NODE_ENV: 'desarrollo',
+            },
 
-    }],
+        }],
 };
