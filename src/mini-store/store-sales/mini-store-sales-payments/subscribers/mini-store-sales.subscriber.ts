fdae9beb72ae40eb35b1863692bdc75ac44acc2f
MisaelMa <amisael.amir.misael@gmail.com>
2020-04-27 16:18:34 -0500

    se agrego la generacion del folio de pago

---
+++ src/mini-store/store-sales/mini-store-sales-payments/subscribers/mini-store-sales.subscriber.ts
@@ -0,0 +1,34 @@
+import { EntitySubscriberInterface, EventSubscriber, getRepository, InsertEvent, UpdateEvent } from 'typeorm';
+import { ColegioDBNameConnection } from '../../../../databases/colegiodb.service';
+import { MiniStoreSalePayment } from '../entities/mini-store-sale-payment.entity';
+
+@EventSubscriber()
+export class MiniStoreSalesPaymentsSubscriber implements EntitySubscriberInterface<MiniStoreSalePayment> {
+    private currentMiniStoreSalesPaymentValue: MiniStoreSalePayment | null = null;
+
+    listenTo() {
+        return MiniStoreSalePayment;
+    }
+
+    async afterInsert(insertEvent: InsertEvent<MiniStoreSalePayment>) {
+        const { entity: payment } = insertEvent;
+        this.generateDocFolio(payment.id);
+    }
+
+    async beforeUpdate(updateEvent: UpdateEvent<MiniStoreSalePayment>) {
+        const { databaseEntity: order } = updateEvent;
+        // this.currentOrder = order;
+    }
+
+    async afterUpdate(updateEvent: UpdateEvent<MiniStoreSalePayment>) {
+        const { entity: order } = updateEvent;
+    }
+
+    async generateDocFolio(id: number): Promise<MiniStoreSalePayment> {
+        const servicePayments = getRepository(MiniStoreSalePayment, ColegioDBNameConnection);
+        const updatePayment = await servicePayments.findOne({ id });
+        console.log('new pago' + updatePayment.folio + ' NTTPA-' + updatePayment.id);
+        updatePayment.folio = 'NTTPA-' + updatePayment.id;
+        return servicePayments.save(updatePayment);
+    }
+}
