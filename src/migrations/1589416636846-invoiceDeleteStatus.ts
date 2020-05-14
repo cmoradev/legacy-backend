b34e6fadbdc070683ca189f6c9853b470d26b251
Jorge <jorge_mejia@outlook.com>
2020-05-14 10:20:17 -0500

    invoice migration

--- src/migrations/1589416636846-invoiceDeleteStatus.ts
+++ src/migrations/1589416636846-invoiceDeleteStatus.ts
@@ -5,7 +5,7 @@ export class invoiceDeleteStatus1589416636846 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("ALTER TABLE `facturacion_empresas` ADD `active` tinyint(1) NOT NULL DEFAULT '1'", undefined);
-    }
+    } 
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("ALTER TABLE `facturacion_empresas` DROP COLUMN `active`", undefined);
