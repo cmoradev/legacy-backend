b89819838add9d1ad10b993aca52b6ae0b308a2c
MisaelMa <amisael.amir.misael@gmail.com>
2020-06-01 18:47:49 -0500

    se elimino una migracion vacia y se agrego las relaciones de los pagos de academias

--- src/academy/charges-academy/academy-charge-payments/academy-charge-payments.controller.ts
+++ src/academy/charges-academy/academy-charge-payments/academy-charge-payments.controller.ts
@@ -2,6 +2,14 @@ import { Controller } from '@nestjs/common';
 import { Crud, CrudController } from '@nestjsx/crud';
 import { AcademyChargePaymentsService } from './academy-charge-payments.service';
 import { AcademyChargePayments } from './entities/academy-charge-payments.entity';
+import { Column, ManyToOne, OneToMany } from 'typeorm';
+import { AcademyCharge } from '../academy-charge/entities/academy-charge.entity';
+import { BranchOffice } from '../../../system/branch-office/entities/branch-office.entity';
+import { BranchOfficeSetting } from '../../../system/branch-office-setting/entities/branch-office-setting.entity';
+import { PaymentStatus } from '../../../common/enums/PaymentStatus';
+import { AcademyChargeMethodsPayments } from '../academy-charge-methods-payments/entities/academy-charge-methods-payments.entity';
+import { User } from '../../../system/users/entities/user.entity';
+import { AcademyChargeInvoice } from '../academy-charge-invoice/entities/academy-charge-invoice.entity';
 
 @Crud({
     model: {
@@ -9,7 +17,16 @@ import { AcademyChargePayments } from './entities/academy-charge-payments.entity
     },
     query: {
         limit: 200,
-        join: {},
+        join: {
+            academyCharge: {},
+            'academyCharge.chargesDetails': {},
+            academyPaymentOffice: {},
+            academyPaymentOfficeSet: {},
+            methodsPayments: {},
+            cashierCharge: {},
+            cashierChargeCancellation: {},
+            academyChargesInvoice: {},
+        },
     },
 })
 @Controller()
