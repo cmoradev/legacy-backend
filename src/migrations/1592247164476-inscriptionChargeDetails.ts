0f622b00748a7f625aeff3e707de75c4f6d2c63a
MisaelMa <amisael.amir.misael@gmail.com>
2020-06-22 12:06:21 -0500

    asdas

--- src/migrations/1592247164476-inscriptionChargeDetails.ts
+++ src/migrations/1592247164476-inscriptionChargeDetails.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class inscriptionChargeDetails1592247164471 implements MigrationInterface {
-    name = 'inscriptionChargeDetails1592247164471'
+export class inscriptionChargeDetails1592247164476 implements MigrationInterface {
+    name = 'inscriptionChargeDetails1592247164476'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("CREATE TABLE `ac_inscrip_concepts_extra_charges` (`id` int NOT NULL AUTO_INCREMENT, `createdAt` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updatedAt` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `version` int NOT NULL DEFAULT 0, `uuid` varchar(36) NOT NULL, `name` varchar(255) NOT NULL, `quantity` int NOT NULL, `applicationType` enum ('1', '2') NULL, `operationType` enum ('sum', 'subtraction', 'division', 'multiplication') NULL, `typeExtraCharge` enum ('1', '2', '3') NULL, `inscChargeDetailId` int NULL, `systemExtraChargesId` int NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
