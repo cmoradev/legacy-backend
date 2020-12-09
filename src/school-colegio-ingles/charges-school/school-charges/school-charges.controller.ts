739652b09452495184a196253aafa53ac82d5869
Edwin Cosme <adriancosme15@gmail.com>
2020-12-09 15:09:41 -0500

    Agregar alias a las rutas del controlador de school-charges.controller.ts

--- src/school-colegio-ingles/charges-school/school-charges/school-charges.controller.ts
+++ src/school-colegio-ingles/charges-school/school-charges/school-charges.controller.ts
@@ -4,41 +4,41 @@ import { SchoolCharge } from './entities/school-charge.entity';
 import { SchoolChargesService } from './school-charges.service';
 
 @Crud({
-    model: {
-        type: SchoolCharge,
-    },
-    query: {
-        limit: 200,
-        join: {
-            schoolCampus: {},
-            schoolCycle: {},
-            cashier: {},
-            cashierCancellation: {},
-            schoolStudent: {},
-            chargesDetails: {},
-            'chargesDetails.extraCharges': {},
-            'chargesDetails.extraCharges.systemExtraCharges': {},
-            chargesPayments: {},
-            'chargesPayments.methodsPayments': {},
-            'chargesPayments.methodsPayments.Bank': {},
-            'chargesPayments.methodsPayments.invoiceMethodPayment': {},
-            'chargesPayments.cashierCharge': {},
-            'chargesPayments.cashierChargeCancellation': {},
-            'chargesPayments.schoolChargesInvoice': {},
-            chargesInvoice: {},
-            'chargesInvoice.agentBilling': {},
-            'chargesInvoice.agentCanceling': {},
-        },
-    },
+	model: {
+		type: SchoolCharge,
+	},
+	query: {
+		limit: 200,
+		join: {
+			schoolCampus: {},
+			schoolCycle: {},
+			cashier: {},
+			cashierCancellation: {},
+			schoolStudent: {},
+			chargesDetails: {},
+			'chargesDetails.extraCharges': {},
+			'chargesDetails.extraCharges.systemExtraCharges': {},
+			chargesPayments: {},
+			'chargesPayments.methodsPayments': { alias: 'chargesPayments_methodsPayments', },
+			'chargesPayments.methodsPayments.Bank': { alias: 'chargesPayments_methodsPayments_Bank', },
+			'chargesPayments.methodsPayments.invoiceMethodPayment': {},
+			'chargesPayments.cashierCharge': {},
+			'chargesPayments.cashierChargeCancellation': {},
+			'chargesPayments.schoolChargesInvoice': {},
+			chargesInvoice: {},
+			'chargesInvoice.agentBilling': {},
+			'chargesInvoice.agentCanceling': {},
+		},
+	},
 })
 @Controller()
 export class SchoolChargesController implements CrudController<SchoolCharge> {
-    constructor(
-        readonly service: SchoolChargesService,
-    ) {
-    }
+	constructor(
+		readonly service: SchoolChargesService,
+	) {
+	}
 
-    get base(): CrudController<SchoolCharge> {
-        return this;
-    }
+	get base(): CrudController<SchoolCharge> {
+		return this;
+	}
 }
