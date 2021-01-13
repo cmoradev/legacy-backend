a457d83dfdf8e119c917442b3f96f0a00dae6815
MisaelMa <amisael.amir.misael@gmail.com>
2021-01-12 20:16:21 -0500

    se agrego la configuracion de capecafe

--- production.config.js
+++ production.config.js
@@ -88,5 +88,20 @@ module.exports = {
             },
 
         },
+        {
+            name: '3100-capecafe',
+            script: 'dist/main.js',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '4G',
+            env_dev: {
+                NODE_ENV: 'development',
+            },
+            env_production: {
+                NODE_ENV: 'capecafe',
+            },
+
+        },
     ],
 };
