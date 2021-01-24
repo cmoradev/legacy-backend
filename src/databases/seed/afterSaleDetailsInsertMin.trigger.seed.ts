2dc337cf3618836e285001dc3f51ba55245482ef
MisaelMa <amisael.amir.misael@gmail.com>
2021-01-23 22:53:00 -0500

    se agrego los seeder de trigers

---
+++ src/databases/seed/afterSaleDetailsInsertMin.trigger.seed.ts
@@ -0,0 +1,22 @@
+import { Factory, Seeder } from 'typeorm-seeding';
+import { Connection } from 'typeorm';
+
+export default class AfterSaleDetailsInsertMinTriggerSeed implements Seeder {
+    public async run(factory: Factory, connection: Connection): Promise<any> {
+        const queryRunner = connection;
+        await queryRunner.query(`DROP TRIGGER IF EXISTS tie_venta_detalle_after_insert`);
+        await queryRunner.query(`
+        CREATE
+        TRIGGER tie_venta_detalle_after_insert AFTER INSERT 
+        ON tie_venta_detalle 
+        FOR EACH ROW BEGIN
+        
+            SET @currentStock = (SELECT stock FROM tie_productos WHERE id = NEW.id_producto);
+            SET @newStock = (@currentStock - NEW.cantidad);
+            
+            UPDATE tie_productos SET stock = @newStock WHERE id = NEW.id_producto;    
+        END
+        `);
+
+    }
+}
---
+++ src/databases/seed/beforeSalePaymentInsert.trigger.seed.ts
@@ -0,0 +1,21 @@
+import { Factory, Seeder } from 'typeorm-seeding';
+import { Connection } from 'typeorm';
+
+export default class BeforeSalePaymentInsertTriggerSeed implements Seeder {
+    public async run(factory: Factory, connection: Connection): Promise<any> {
+        const queryRunner = connection;
+        await queryRunner.query(`DROP TRIGGER IF EXISTS before_venta_pago_insert`);
+        await queryRunner.query(`
+        CREATE TRIGGER before_venta_pago_insert 
+            BEFORE INSERT ON tie_venta_pagos
+            FOR EACH ROW  
+            BEGIN 
+                SET @prefix = (SELECT foliaje_pago FROM facturacion_empresas WHERE id= NEW.storePaymentOfficeSetId AND active = true); 
+                SET @consecutive = (SELECT serie_pago FROM facturacion_empresas WHERE id= NEW.storePaymentOfficeSetId AND active = true); 
+                SET @consecutive = (SELECT @consecutive + 1); SET @folio = (CONCAT_WS('-', @prefix, @consecutive)); 
+                SET NEW.folio = @folio; UPDATE facturacion_empresas SET serie_pago = @consecutive WHERE id= NEW.storePaymentOfficeSetId AND active = true;
+                
+            END`);
+
+    }
+}
