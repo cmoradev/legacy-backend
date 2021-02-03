fe31e44defe4a264cf1101e510796fddc940b7b0
MisaelMa <amisael.amir.misael@gmail.com>
2021-02-02 21:30:57 -0500

    se creo un ecosistema para salgo de prueba

--- production.config.js
+++ production.config.js
@@ -15,21 +15,6 @@ module.exports = {
             },
         },
         {
-            name: '4500-tortilleria',
-            script: 'dist/main.js',
-            instances: 1,
-            autorestart: true,
-            watch: false,
-            max_memory_restart: '4G',
-            env_dev: {
-                NODE_ENV: 'development',
-            },
-            env_production: {
-                NODE_ENV: 'tortilleria',
-            },
-
-        },
-        {
             name: '6500-pachamama',
             script: 'dist/main.js',
             instances: 1,
