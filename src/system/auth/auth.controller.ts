d2fe86ea7c7b4b118775a73c69696d852c28dec9
Edwin Cosme <adriancosme15@gmail.com>
2021-02-05 10:39:23 -0500

    Recover password feature

--- src/system/auth/auth.controller.ts
+++ src/system/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Req, Res, UnauthorizedException, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UnauthorizedException,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { RegisterGuard } from './guards/register.guard';
 import { AuthGuard } from '@nestjs/passport';
@@ -9,85 +19,103 @@ import { LocalAuthGuard } from './guards/login.guard';
 import { AuthAccessTokensService } from '../auth-access-tokens/auth-access-tokens.service';
 import * as moment from 'moment';
 import { RefreshGuard } from './guards/refresh.guard';
+import { ForgotPasswordDto } from './dto/forgot-password.dto';
+import { JwtService, JwtVerifyOptions } from '@nestjs/jwt';
 
 
 @Controller()
 export class AuthController {
-    constructor(readonly authService: AuthService,
-                readonly authAccessTokensService: AuthAccessTokensService,
-                readonly settingsService: SettingsService) {
+  constructor(readonly authService: AuthService,
+              readonly authAccessTokensService: AuthAccessTokensService,
+              readonly settingsService: SettingsService,
+              readonly jwtService: JwtService) {
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Req() req, @Res() res: Response) {
+    const company = await this.settingsService.fetchCompany();
+    const jwt = await this.authService.generateJWT(req.user);
+    await this.authAccessTokensService.saveToken({
+      expiresAt: moment(jwt.decode.exp * 1000).toDate(),
+      name: 'Token',
+      revoked: false,
+      jwt: jwt.access_token,
+      isActive: true,
+      refresh: false,
+      scopes: '[]',
+      clientId: 1,
+      userId: req.user.id,
+    });
+
+    res.status(200);
+    res.send({
+      user: req.user,
+      accessJWT: {
+        access_token: jwt.access_token,
+      },
+      company,
+    });
+  }
+
+  @UseGuards(RegisterGuard)
+  @Post('register')
+  async register(@Req() req, @Res() res: Response) {
+    res.status(200);
+    res.send(req.user);
+  }
+
+  @UseGuards(RefreshGuard)
+  @Post('refresh-token')
+  async refresToken(@Req() req, @Res() res: Response) {
+    const { token, sub, username } = req.user;
+
+    const lastToken = await this.authAccessTokensService.findToken(token);
+    if (lastToken) {
+
+      const jwt = await this.authService.generateJWT({ id: sub, email: username });
+      lastToken.jwt = jwt.access_token;
+      lastToken.expiresAt = moment(jwt.decode.exp * 1000).toDate();
+      lastToken.refresh = true;
+      this.authAccessTokensService.repo.save(lastToken);
+      res.status(200);
+      res.send({
+        refresh_token: jwt.access_token,
+      });
     }
+    throw new UnauthorizedException('Not Found Token');
+  }
 
-    @UseGuards(LocalAuthGuard)
-    @Post('login')
-    async login(@Req() req, @Res() res: Response) {
-        const company = await this.settingsService.fetchCompany();
-        const jwt = await this.authService.generateJWT(req.user);
-        await this.authAccessTokensService.saveToken({
-            expiresAt: moment(jwt.decode.exp * 1000).toDate(),
-            name: 'Token',
-            revoked: false,
-            jwt: jwt.access_token,
-            isActive: true,
-            refresh: false,
-            scopes: '[]',
-            clientId: 1,
-            userId: req.user.id,
-        });
+  // @UseGuards(SessionGuard)
+  @Get('logout')
+  public logout(@Req() req, @Res() res) {
+    res.send(req);
+  }
 
-        res.status(200);
-        res.send({
-            user: req.user,
-            accessJWT: {
-                access_token: jwt.access_token,
-            },
-            company,
-        });
+  @UseGuards(AuthGuard('jwt'))
+  @Post('jwt')
+  public getJWT(@Req() req, @Res() res: Response) {
+    res.status(201).json(req.user);
+  }
+
+  @UseGuards(JwtGuard)
+  @Get('me')
+  public checkMySession(@Req() req, @Res() res: Response) {
+    res.status(201).json(req.user);
+  }
+
+  @Post('forgot-password')
+  public async forgotPassword(@Body(new ValidationPipe()) forgotPassword: ForgotPasswordDto): Promise<void> {
+    await this.authService.forgotPassword(forgotPassword);
+  }
+
+  @Get('reset-password')
+  public resetPassword(@Req() req, @Res() res: Response) {
+    try {
+      const token = this.jwtService.verify(req.query.key);
+      res.status(200).send(token);
+    } catch (e) {
+      res.status(401).send({ data: { statusCode: 401, message: e.message } });
     }
-
-    @UseGuards(RegisterGuard)
-    @Post('register')
-    async register(@Req() req, @Res() res: Response) {
-        res.status(200);
-        res.send(req.user);
-    }
-
-    @UseGuards(RefreshGuard)
-    @Post('refresh-token')
-    async refresToken(@Req() req, @Res() res: Response) {
-        const { token, sub, username } = req.user;
-
-        const lastToken = await this.authAccessTokensService.findToken(token);
-        if (lastToken) {
-
-            const jwt = await this.authService.generateJWT({ id: sub, email: username });
-            lastToken.jwt = jwt.access_token;
-            lastToken.expiresAt = moment(jwt.decode.exp * 1000).toDate();
-            lastToken.refresh = true;
-            this.authAccessTokensService.repo.save(lastToken);
-            res.status(200);
-            res.send({
-                refresh_token: jwt.access_token,
-            });
-        }
-        throw new UnauthorizedException('Not Found Token');
-    }
-
-    // @UseGuards(SessionGuard)
-    @Get('logout')
-    public logout(@Req() req, @Res() res) {
-        res.send(req);
-    }
-
-    @UseGuards(AuthGuard('jwt'))
-    @Post('jwt')
-    public getJWT(@Req() req, @Res() res: Response) {
-        res.status(201).json(req.user);
-    }
-
-    @UseGuards(JwtGuard)
-    @Get('me')
-    public checkMySession(@Req() req, @Res() res: Response) {
-        res.status(201).json(req.user);
-    }
+  }
 }
