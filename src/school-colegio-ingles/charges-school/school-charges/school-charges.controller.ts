3c7c2bbbd285994dd8a9f4ea3bb0febeed7d206b
MisaelMa <amisael.amir.misael@gmail.com>
2021-03-26 11:39:06 -0500

    se cambio el nombre de la subrelacion

--- src/school-colegio-ingles/charges-school/school-charges/school-charges.controller.ts
+++ src/school-colegio-ingles/charges-school/school-charges/school-charges.controller.ts
@@ -20,7 +20,7 @@ import { JwtGuard } from '../../../system/auth/guards/jwt.guard';
       schoolStudent: {},
       chargesDetails: {},
       'chargesDetails.extraCharges': {},
-      'chargesDetails.schoolPayment':{},
+      'chargesDetails.schoolPlanPayment':{},
       'chargesDetails.extraCharges.systemExtraCharges': {},
       chargesPayments: {},
       'chargesPayments.methodsPayments': { alias: 'chargesPayments_methodsPayments' },
