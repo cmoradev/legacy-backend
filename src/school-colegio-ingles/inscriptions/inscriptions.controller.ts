749a8aa539dc90fabf58f6ef388478755b823885
MisaelMa <amisael.amir.misael@gmail.com>
2021-03-26 15:59:02 -0500

    se agrego la consulta de los datos para generar el reporte

--- src/school-colegio-ingles/inscriptions/inscriptions.controller.ts
+++ src/school-colegio-ingles/inscriptions/inscriptions.controller.ts
@@ -7,85 +7,105 @@ import xlsx from 'node-xlsx';
 import * as path from 'path';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { VerifyregistratioDto } from './dto/verifyregistratio.dto';
-import { VerificarInscriprions } from './interfaces/inscriptions.interface';
+import { Attendance, VerificarInscriprions } from './interfaces/inscriptions.interface';
 import { ExcelSheet } from '../../common/office/sheets/interfaces/excel.interface';
 import { sheetToObjPage } from '../../common/office/sheets';
 import { Response } from 'express';
 import { JwtGuard } from '../../system/auth/guards/jwt.guard';
 import { Student } from '../students/entities/student.entity';
+import { QueryBilling } from '../../mini-store/store-sales/mini-store-sales-payments/interface/InvoiceMiniStore.interface';
 
 @UseGuards(JwtGuard)
 @Crud({
-  model: {
-    type: Inscription,
-  },
-  query: {
-    maxLimit: 200,
-    join: {
-      inscripStudent: {},
-      inscripCampus: {},
-      inscripGrade: {},
-      inscripGroup: {},
-      inscripLevel: {},
-      inscripCycle: {},
-      inscripAgentCreator: {},
-      inscripAgentEditor: {},
-      inscripClassroom: {},
-      paymentPlan: {},
-      inscripAssignmentsInscription: {},
-      inscripStudyPlanVariant: {},
-      inscripStudyPlan: {},
-      schoolPayments: { alias: 'schoolPayments' },
-      'schoolPayments.paymentPlanConcept': { alias: 'paymentPlanConcepts' },
-      'schoolPayments.schoolChargeDetail': { alias: 'schoolChargesDetails' },
-      'schoolPayments.schoolChargeDetail.extraCharges': { alias: 'extraCharges' },
-      'schoolPayments.extraCharges': {},
+    model: {
+        type: Inscription,
     },
-  },
+    query: {
+        maxLimit: 200,
+        join: {
+            inscripStudent: {},
+            inscripCampus: {},
+            inscripGrade: {},
+            inscripGroup: {},
+            inscripLevel: {},
+            inscripCycle: {},
+            inscripAgentCreator: {},
+            inscripAgentEditor: {},
+            inscripClassroom: {},
+            paymentPlan: {},
+            inscripAssignmentsInscription: {},
+            inscripStudyPlanVariant: {},
+            inscripStudyPlan: {},
+            schoolPayments: { alias: 'schoolPayments' },
+            'schoolPayments.paymentPlanConcept': { alias: 'paymentPlanConcepts' },
+            'schoolPayments.schoolChargeDetail': { alias: 'schoolChargesDetails' },
+            'schoolPayments.schoolChargeDetail.extraCharges': { alias: 'extraCharges' },
+            'schoolPayments.extraCharges': {},
+        },
+    },
 })
 @Controller()
 export class InscriptionsController implements CrudController<Inscription> {
-  constructor(
-    readonly service: InscriptionsService,
-  ) {
-  }
-
-  get base(): CrudController<Inscription> {
-    return this;
-  }
-
-  @Get('/amir')
-  async verify(): Promise<any> {
-    return 'amir';
-  }
-
-  @Get('/add/inscripciones')
-  public async addInsccripciones(@Req() req, @Res() res: Response) {
-    try {
-      // @ts-ignore
-      // const data = await this.service.repo.save([] as unknown as Inscription[]);
-      res.send({ save: true });
-    } catch (e) {
-      res.send(e);
+    constructor(
+        readonly service: InscriptionsService,
+    ) {
     }
 
-  }
+    get base(): CrudController<Inscription> {
+        return this;
+    }
 
-  @Post('/verifyinscription')
-  @UseInterceptors(FileInterceptor('file', {
-    storage: diskStorage({
-      destination: (req, file, cb) => {
-        cb(null, './uploads');
-      },
-      filename: (req, file, cb) => {
-        cb(null, Date.now() + '_' + file.originalname);
-      },
-    }),
-  }))
-  async verifyregistration(@UploadedFile() file, @Body() data: VerifyregistratioDto): Promise<any> {
-    const pathfile = path.join(__dirname, `../../../uploads/` + file.filename);
-    const Sheets: ExcelSheet[] = xlsx.parse(pathfile);
-    const inscripcion: VerificarInscriprions = await sheetToObjPage(Sheets);
-    return await this.service.verificarInscription(inscripcion, data);
-  }
+    @Get('/amir')
+    async verify(): Promise<any> {
+        return 'amir';
+    }
+
+    @Get('/add/inscripciones')
+    public async addInsccripciones(@Req() req, @Res() res: Response) {
+        try {
+            // @ts-ignore
+            // const data = await this.service.repo.save([] as unknown as Inscription[]);
+            res.send({ save: true });
+        } catch (e) {
+            res.send(e);
+        }
+
+    }
+
+    @Get('/report/attendance')
+    public async attendance(@Body() query: Attendance, @Req() req, @Res() res: Response) {
+        try {
+            const result = {
+                data: [],
+                file: '',
+            };
+            if (query.onlyFile) {
+                result.data = await this.service.reportAttendance(query)
+            } else {
+
+            }
+            res.send(query);
+        } catch (e) {
+            res.send(e);
+        }
+
+    }
+
+    @Post('/verifyinscription')
+    @UseInterceptors(FileInterceptor('file', {
+        storage: diskStorage({
+            destination: (req, file, cb) => {
+                cb(null, './uploads');
+            },
+            filename: (req, file, cb) => {
+                cb(null, Date.now() + '_' + file.originalname);
+            },
+        }),
+    }))
+    async verifyregistration(@UploadedFile() file, @Body() data: VerifyregistratioDto): Promise<any> {
+        const pathfile = path.join(__dirname, `../../../uploads/` + file.filename);
+        const Sheets: ExcelSheet[] = xlsx.parse(pathfile);
+        const inscripcion: VerificarInscriprions = await sheetToObjPage(Sheets);
+        return await this.service.verificarInscription(inscripcion, data);
+    }
 }
--- src/school-colegio-ingles/inscriptions/inscriptions.service.ts
+++ src/school-colegio-ingles/inscriptions/inscriptions.service.ts
@@ -4,67 +4,99 @@ import { Inscription } from './entities/inscription.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Student } from '../students/entities/student.entity';
-import { VerificarInscriprions } from './interfaces/inscriptions.interface';
+import { Attendance, VerificarInscriprions } from './interfaces/inscriptions.interface';
 import { VerifyregistratioDto } from './dto/verifyregistratio.dto';
 import { ColegioDBNameConnection } from '../../databases/colegiodb.service';
 
 @Injectable()
 export class InscriptionsService extends TypeOrmCrudService<Inscription> {
-  constructor(
-    @InjectRepository(Inscription, ColegioDBNameConnection) readonly repo: Repository<Inscription>,
-    @InjectRepository(Student, ColegioDBNameConnection) readonly student: Repository<Student>,
-
-  ) {
-    super(repo);
-  }
-
-  public async verificarInscription(data: VerificarInscriprions, datainsc: VerifyregistratioDto): Promise<any> {
-  /*  const result: any = {
-      registered: [],
-      notregistered: [],
-      nonstudent: [],
-      enrolledStudents: [],
-    };
-    result.enrolledStudents = await this.inscriptions.find({
-      relations: ['student'],
-      where: {
-        idPlantel: datainsc.plantel,
-        idLevel: datainsc.nivel,
-        idGrade: datainsc.grado,
-        idGroup: datainsc.grupo,
-        idCycle: datainsc.ciclo,
-      },
-    });
-
-    for (const student of data.data) {
-
-      const studentR: any = await this.student.findOne({ matricula: student.matricula });
-      if (studentR) {
-        const inscripcionalumno = await this.inscriptions.findOne({
-            idStudent: studentR.id,
-            idPlantel: datainsc.plantel,
-            idLevel: datainsc.nivel,
-            idGrade: datainsc.grado,
-            idGroup: datainsc.grupo,
-            idCycle: datainsc.ciclo,
-          },
-        );
-
-        if (inscripcionalumno) {
-          studentR.inscripcion = inscripcionalumno;
-          // inscritor y registrado en el grupo grado nivel
-          result.registered.push(studentR);
-        } else {
-          // regitrado pero no inscrito en la seecion selecionada
-          result.notregistered.push(studentR);
-        }
-      } else {
-        // no regitrado en el sistema
-        console.log(student)
-        result.nonstudent.push(student);
-      }
-
+    constructor(
+        @InjectRepository(Inscription, ColegioDBNameConnection) readonly repo: Repository<Inscription>,
+        @InjectRepository(Student, ColegioDBNameConnection) readonly student: Repository<Student>,
+    ) {
+        super(repo);
     }
-    return result;*/
-  }
+
+    public async reportAttendance(query: Attendance) {
+        const inscripcion = this.repo.createQueryBuilder('inscription')
+            .leftJoinAndSelect('inscription.inscripStudent', 'inscripStudent')
+            .leftJoinAndSelect('inscription.inscripCampus', 'inscripCampus')
+            .leftJoinAndSelect('inscription.inscripCycle', 'inscripCycle')
+            .leftJoinAndSelect('inscription.inscripLevel', 'inscripLevel')
+            .leftJoinAndSelect('inscription.inscripGrade', 'inscripGrade')
+            .leftJoinAndSelect('inscription.inscripClassroom', 'inscripClassroom');
+
+        inscripcion.where('inscripCampus.id= :officeId', {
+            officeId: query.branchOfficeId,
+        });
+
+        inscripcion.andWhere('inscripCycle.id = :cycleId', {
+            cycleId: query.cycleId,
+        });
+
+        inscripcion.andWhere('inscripLevel.id = :levelId', {
+            levelId: query.levelId,
+        });
+
+        inscripcion.andWhere('inscripGrade.id = :gradeId', {
+            gradeId: query.gradeId,
+        });
+        if (query.classRoomId !== 0) {
+            inscripcion.andWhere('inscripClassroom.id = :classroomId', {
+                classroomId: query.classRoomId,
+            });
+        }
+
+        return await inscripcion.getMany();
+    }
+
+    public async verificarInscription(data: VerificarInscriprions, datainsc: VerifyregistratioDto): Promise<any> {
+        /*  const result: any = {
+            registered: [],
+            notregistered: [],
+            nonstudent: [],
+            enrolledStudents: [],
+          };
+          result.enrolledStudents = await this.inscriptions.find({
+            relations: ['student'],
+            where: {
+              idPlantel: datainsc.plantel,
+              idLevel: datainsc.nivel,
+              idGrade: datainsc.grado,
+              idGroup: datainsc.grupo,
+              idCycle: datainsc.ciclo,
+            },
+          });
+
+          for (const student of data.data) {
+
+            const studentR: any = await this.student.findOne({ matricula: student.matricula });
+            if (studentR) {
+              const inscripcionalumno = await this.inscriptions.findOne({
+                  idStudent: studentR.id,
+                  idPlantel: datainsc.plantel,
+                  idLevel: datainsc.nivel,
+                  idGrade: datainsc.grado,
+                  idGroup: datainsc.grupo,
+                  idCycle: datainsc.ciclo,
+                },
+              );
+
+              if (inscripcionalumno) {
+                studentR.inscripcion = inscripcionalumno;
+                // inscritor y registrado en el grupo grado nivel
+                result.registered.push(studentR);
+              } else {
+                // regitrado pero no inscrito en la seecion selecionada
+                result.notregistered.push(studentR);
+              }
+            } else {
+              // no regitrado en el sistema
+              console.log(student)
+              result.nonstudent.push(student);
+            }
+
+          }
+          return result;*/
+    }
 }
