1bb914136cfaf26e2e499edd825ee073a467f5f8
Caleb Isaac Mora Diaz <calexico659@gmail.com>
2021-06-03 10:58:50 -0500

    Add ecosystem salmo 23

---
+++ tortilleria.salmo.23.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: '4501-salmo-tortilleria',
+      script: 'dist/main.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '4G',
+      env_dev: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'tortilleria.salmo',
+      },
+
+    },
+  ],
+};
