01014e317a6584b3b356ab89cae10f6d30b6dd81
Caleb Isaac Mora Díaz <Calexico659@Gmail.com>
2021-11-25 11:31:15 -0500

    fix - search new matricula and egenrate news before_students_insert tigger

--- src/school-colegio-ingles/students/seed/BeforeInsertStudent.trigger.seed.ts
+++ src/school-colegio-ingles/students/seed/BeforeInsertStudent.trigger.seed.ts
@@ -11,15 +11,27 @@ export default class BeforeInsertStudentTriggerSeed implements Seeder {
         FOR EACH ROW
             BEGIN
             DECLARE prefix VARCHAR(20);
-            SET @consecutive = (SELECT COUNT(*) AS total FROM alumnos WHERE id_modalidad = NEW.id_modalidad);
+            DECLARE nuevaMatricula VARCHAR(20);
+            DECLARE consecutive INT(11);
+
+            SET consecutive = (SELECT COUNT(*) AS total FROM alumnos WHERE id_modalidad = NEW.id_modalidad) + 1;
 
             IF NEW.id_modalidad = 1 THEN SET prefix = 'ALM';
+
             ELSEIF NEW.id_modalidad = 2 THEN SET prefix = 'CLT';
+
             ELSE SET prefix = 'PRO';
+
             END IF;
             
-            SET @matricula = (CONCAT_WS('-', prefix, (@consecutive + 1))); 
-            SET NEW.matricula = @matricula; 
+            SET nuevaMatricula = (CONCAT_WS('-', prefix, (consecutive))); 
+
+			WHILE (SELECT COUNT(*) AS total FROM alumnos WHERE matricula = nuevaMatricula) > 0 DO
+				SET consecutive = consecutive + 1;
+                SET nuevaMatricula = (CONCAT_WS('-', prefix, (consecutive))); 
+			END WHILE;
+            
+            SET NEW.matricula = nuevaMatricula; 
        
 	    END`);
 
