f2c5faeba8c2608342301f0458ec8058cd4f2908
MisaelMa <amisael.amir.misael@gmail.com>
2022-03-12 17:02:11 -0500

    feat(type): se agrego las nuevas propiedades de typados

--- src/mini-store/store-sales/mini-store-invoices/dto/cancel.invoice.sw.dto.ts
+++ src/mini-store/store-sales/mini-store-invoices/dto/cancel.invoice.sw.dto.ts
@@ -3,6 +3,8 @@ export class CancelInvoiceSwDto {
     uuid: string;
     invoiceId: number;
     reason: string;
+    movito: '01' | '02' | '03' | '04';
+    folioSustitucion: string;
     sendMail: boolean;
     subject: string;
     body: string;
