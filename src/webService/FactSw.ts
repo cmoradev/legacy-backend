3fc16286b2f2caedfeefd67b6262bd0c86387cbd
Caleb Isaac Mora Diaz <calexico659@gmail.com>
2022-03-14 15:00:29 -0500

    fix - credential and token sw

--- src/webService/FactSw.ts
+++ src/webService/FactSw.ts
@@ -4,13 +4,12 @@ import * as CancelationRequest from 'sw-sdk-nodejs/lib/SWServices/Cancelation/Ca
 export class FactSw {
   private tokenProd: string = 'T2lYQ0t4L0RHVkR4dHZ5Nkk1VHNEakZ3Y0J4Nk9GODZuRyt4cE1wVm5tbXB3YVZxTHdOdHAwVXY2NTdJb1hkREtXTzE3dk9pMmdMdkFDR2xFWFVPUXpTUm9mTG1ySXdZbFNja3FRa0RlYURqbzdzdlI2UUx1WGJiKzViUWY2dnZGbFloUDJ6RjhFTGF4M1BySnJ4cHF0YjUvbmRyWWpjTkVLN3ppd3RxL0dJPQ.T2lYQ0t4L0RHVkR4dHZ5Nkk1VHNEakZ3Y0J4Nk9GODZuRyt4cE1wVm5tbFlVcU92YUJTZWlHU3pER1kySnlXRTF4alNUS0ZWcUlVS0NhelhqaXdnWTRncklVSWVvZlFZMWNyUjVxYUFxMWFxcStUL1IzdGpHRTJqdS9Zakw2UGRZc1hLcDVaMVNtMmJ3L2xhMEdHVzdWZHV1QXJ6V0JjSjhvSEhwY1VtcHA3L1hoSDFvWlhzVSt5cjh4Z3M4VWxuWmRXV05Cb1ZjR3ova2R5Sko1anhQSXNmZXFGTjFic043NzdFUTBPTFdINmsxRnJRa0VTVFI3UTBKb3EybkR3aDhSOGF2c2g1UHU2RHFRVy9Pb3MvVEt3dWN6SHo5azhzWFcrU0dxTGFHK29neSt0aG80NlpqY2d2VklBMDlPUmZmOEVrbUVVcHY2Nk9vOHN5QVpVZVVYY0tvblFlUWZWMklyTGNXRFozUXBLTWJWSGZWVjlpY0owanRiVGVNNENkcUdOdW5lYks1b1BPREF5V1pnNGMrOXU5UTVXNHcvNStkYnorYW00M2M0OFpEZXBZa2VYSUhlUDh1eUdvemlGODhXNEJTa3ZRNVQ2bXFvUVYrencwamZrczBoM0Z2UEsxS3kzZHo1YTAvbkJtaWwxeW1jRjVRNFhSaWNOS2xKUDUwM01mS1hzcHBrYmxkTWFHMDhXa25RPT0.VfgUe8ZmsEsT7KhwCxjaEk80k1xucBX9BiWODKhrbxc';
   private tokenDev: string = 'T2lYQ0t4L0RHVkR4dHZ5Nkk1VHNEakZ3Y0J4Nk9GODZuRyt4cE1wVm5tbXB3YVZxTHdOdHAwVXY2NTdJb1hkREtXTzE3dk9pMmdMdkFDR2xFWFVPUXpTUm9mTG1ySXdZbFNja3FRa0RlYURqbzdzdlI2UUx1WGJiKzViUWY2dnZGbFloUDJ6RjhFTGF4M1BySnJ4cHF0YjUvbmRyWWpjTkVLN3ppd3RxL0dJPQ.T2lYQ0t4L0RHVkR4dHZ5Nkk1VHNEakZ3Y0J4Nk9GODZuRyt4cE1wVm5tbFlVcU92YUJTZWlHU3pER1kySnlXRTF4alNUS0ZWcUlVS0NhelhqaXdnWTRncklVSWVvZlFZMWNyUjVxYUFxMWFxcStUL1IzdGpHRTJqdS9Zakw2UGRmdmNpWlVpNkY5VXhHb3RRZDBnd2hMRm1HSWtmUGloSk5mbjdpNlZnVUp6RmhzemtFUVh4b2FnS3poTFBTU1Q0YmEvSVM2RHU1V0svWGdHWmdPWFBjcjhXeFdOVTNsSnFZM0drREJQb2NWTDdyNHhlTDJicnp6NEVaUDB4YzJBeHFHVDZsR2IwTXFXOHRmU2s2SFNzbnhpeCt6UFhRc2x1ZDFBVytiUHpJUUM1WndNZTlsNVdjYWlrUjRaYWY4SmtBQktWNFNsazVybG8zNkwwV3g0Y29aakZ5M29EZzVlOXBwTWpVM1FJUmpOeWhGVnpxSzRsVjBiVjlybFJDditPc2hzZHlrVEN0aWkwOThhUmVVdVJTWk8yUmpyeDlnQTMvZm1ldG5za2VNMlVLTjU1MlFJVDNXU09aK0J0VWd4NzRIM0Qvd1BxUk4yNlFJWmdJZWRCdEoyUjNoT1VmN2IxUnFjYjM4ajM4YXFYMjFWME1DV0pJK1g4b205ODdUaDc.K5KzDuUZw_IhYh78F8uA-Aj80cKiT15w_wS9sEFJXmI';
-  private url = process.env.NODE_ENV === 'development' ? 'http://services.test.sw.com.mx' : 'https://services.sw.com.mx';
+  private url = process.env.NODE_ENV === 'development' ? 'https://services.test.sw.com.mx' : 'https://services.sw.com.mx';
   private user = process.env.NODE_ENV === 'development' ? 'caleb@munyaal.app' : 'developers@colegioinglesplaya.com';
   private password = process.env.NODE_ENV === 'development' ? 'Munyaal+SW' : 'S0p0rt3.01';
   private token = process.env.NODE_ENV === 'development' ? this.tokenDev : this.tokenProd;
 
-  constructor() {
-  }
+  constructor() {}
 
   getToken() {
     return new Promise((resolve, reject) => {
@@ -34,16 +33,17 @@ export class FactSw {
 
   public async facturar(xml: string): Promise<StampV4> {
     return new Promise((resolve, reject) => {
-      console.log(xml);
       const obj = {
         url: this.url,
         user: this.user,
         password: this.password,
       };
+      console.log(xml);
+      console.log(obj);
       const stamp = StampService.Set(obj);
       stamp.StampV4(xml, (err, data) => {
         if (err) {
-          console.log("method Facturar", err)
+          console.log(err)
           const errRes = {
             status: 'error',
             message: err.message,
