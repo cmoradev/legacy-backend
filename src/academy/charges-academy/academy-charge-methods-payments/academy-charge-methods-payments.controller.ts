292a13a88e20714319e005e26a552814340e9e05
Francisco Delgado <2005040@utrivieramaya.edu.mx>
2022-10-01 12:37:35 -0500

    feat - se modifica el crud en limit; tambien se elimina el import jwtguard

--- src/academy/charges-academy/academy-charge-methods-payments/academy-charge-methods-payments.controller.ts
+++ src/academy/charges-academy/academy-charge-methods-payments/academy-charge-methods-payments.controller.ts
@@ -1,8 +1,7 @@
-import { Controller, Delete, Param, ParseIntPipe, Put, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Param, ParseIntPipe, Put } from '@nestjs/common';
 import { Crud, CrudController } from '@nestjsx/crud';
 import { AcademyChargeMethodsPaymentsService } from './academy-charge-methods-payments.service';
 import { AcademyChargeMethodsPayments } from './entities/academy-charge-methods-payments.entity';
-import { JwtGuard } from '../../../system/auth/guards/jwt.guard';
 
 @Crud({
     model: {
@@ -14,7 +13,7 @@ import { JwtGuard } from '../../../system/auth/guards/jwt.guard';
                 $eq: null,
             },
         },
-        limit: 200,
+        limit: 10,
         join: {},
     },
 })
