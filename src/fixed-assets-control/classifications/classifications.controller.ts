90cb1b5c7779c01fa57bcd57efc0ec871dbe6f32
Francisco Delgado <2005040@utrivieramaya.edu.mx>
2022-10-01 13:42:08 -0500

    feat - se elimina el import jwtguard

--- src/fixed-assets-control/classifications/classifications.controller.ts
+++ src/fixed-assets-control/classifications/classifications.controller.ts
@@ -1,8 +1,7 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { Crud, CrudController } from '@nestjsx/crud';
 import { Classification } from './entities/classification.entity';
 import { ClassificationsService } from './classifications.service';
-import { JwtGuard } from '../../system/auth/guards/jwt.guard';
 
 @Crud({
     model: {
