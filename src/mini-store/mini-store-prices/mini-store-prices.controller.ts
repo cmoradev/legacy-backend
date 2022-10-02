4f849f6a16c309011973ee406f75d902ddc86abc
Francisco Delgado <2005040@utrivieramaya.edu.mx>
2022-10-02 14:21:40 -0500

    feat - se elimino el import jwtguard

--- src/mini-store/mini-store-prices/mini-store-prices.controller.ts
+++ src/mini-store/mini-store-prices/mini-store-prices.controller.ts
@@ -1,6 +1,5 @@
 import { Crud, CrudController } from '@nestjsx/crud';
-import { Controller, UseGuards, Delete, Param, ParseIntPipe, Put } from '@nestjs/common';
-import { JwtGuard } from '../../system/auth/guards/jwt.guard';
+import { Controller, Delete, Param, ParseIntPipe, Put } from '@nestjs/common';
 import { MiniStorePrices } from './entities/mini-store-prices.entity';
 import { MiniStorePricesService } from './mini-store-prices.service';
 
