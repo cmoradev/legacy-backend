b2ee1412ab3dcd1dc80bafc5cedf31d7ed70fde1
Francisco Delgado <2005040@utrivieramaya.edu.mx>
2022-10-02 14:28:39 -0500

    feat - se modifica el crud en join; tambien se elimina el import jwtguard

--- src/mini-store/mini-store-prices-lists/mini-store-prices-lists.controller.ts
+++ src/mini-store/mini-store-prices-lists/mini-store-prices-lists.controller.ts
@@ -1,8 +1,7 @@
-import { Controller, Delete, Param, ParseIntPipe, Put, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Param, ParseIntPipe, Put } from '@nestjs/common';
 import { MiniStorePricesListsService } from './mini-store-prices-lists.service';
 import { Crud, CrudController } from '@nestjsx/crud';
 import { MiniStorePriceList } from './entities/mini-store-price-list.entity';
-import { JwtGuard } from '../../system/auth/guards/jwt.guard';
 
 @Crud({
     model: {
@@ -15,8 +14,8 @@ import { JwtGuard } from '../../system/auth/guards/jwt.guard';
             },
         },
         join: {
-            storeProducts: {},
-            branchOfficeList: {},
+            storeProducts: {eager: false},
+            branchOfficeList: {eager: false},
         },
     },
 })
