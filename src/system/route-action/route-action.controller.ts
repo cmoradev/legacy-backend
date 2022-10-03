75721c5397b88a16152cf2bcdea60dfd7965d786
Francisco Delgado <2005040@utrivieramaya.edu.mx>
2022-10-02 19:24:35 -0500

    feat - se modifica el crud en join; tambien se elimina el import jwtguard

--- src/system/route-action/route-action.controller.ts
+++ src/system/route-action/route-action.controller.ts
@@ -1,15 +1,15 @@
-import { Controller, UseGuards } from '@nestjs/common';
-import { JwtGuard } from '../auth/guards/jwt.guard';
+import { Controller } from '@nestjs/common';
 import { Crud, CrudController } from '@nestjsx/crud';
 import { RouteAction } from './entities/route-action.entity';
 import { RouteActionService } from './route-action.service';
+
 @Crud({
     model: {
         type: RouteAction,
     },
     query: {
         join: {
-            actions: {},
+            actions: {eager: false},
         },
     },
 })
