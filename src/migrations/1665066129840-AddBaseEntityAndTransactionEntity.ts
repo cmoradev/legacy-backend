c2598eb21f51b9c213a501392bd6dfdd6ac91791
Caleb Isaac Mora Diaz <calexico659@gmail.com>
2022-10-12 16:43:25 -0500

    feat - se aceptan valores nulos en los datos fiscales

--- src/migrations/1665066129840-AddBaseEntityAndTransactionEntity.ts
+++ src/migrations/1665066129840-AddBaseEntityAndTransactionEntity.ts
@@ -9,9 +9,9 @@ export class AddBaseEntityAndTransactionEntity1665066129840 implements Migration
         await queryRunner.query("ALTER TABLE `credit_note_school` ADD `version` int NOT NULL DEFAULT '0'");
         await queryRunner.query("ALTER TABLE `credit_note_store` ADD `version` int NOT NULL DEFAULT '0'");
         await queryRunner.query("ALTER TABLE `alumnos` ADD `saldo` int NULL DEFAULT '0'");
-        await queryRunner.query("ALTER TABLE `familia_infofiscal` CHANGE `domicilioFiscalReceptor` `domicilioFiscalReceptor` varchar(5) NOT NULL");
-        await queryRunner.query("ALTER TABLE `familia_infofiscal` CHANGE `regimenFiscalReceptor` `regimenFiscalReceptor` varchar(255) NOT NULL");
-        await queryRunner.query("ALTER TABLE `familia_infofiscal` CHANGE `keyRegimen` `keyRegimen` varchar(255) NOT NULL");
+        await queryRunner.query("ALTER TABLE `familia_infofiscal` CHANGE `domicilioFiscalReceptor` `domicilioFiscalReceptor` varchar(5) NULL");
+        await queryRunner.query("ALTER TABLE `familia_infofiscal` CHANGE `regimenFiscalReceptor` `regimenFiscalReceptor` varchar(255) NULL");
+        await queryRunner.query("ALTER TABLE `familia_infofiscal` CHANGE `keyRegimen` `keyRegimen` varchar(255) NULL");
         await queryRunner.query("ALTER TABLE `transaction` ADD CONSTRAINT `FK_3e2a82e1e69f6e979bec0c3a6d6` FOREIGN KEY (`studentId`) REFERENCES `alumnos`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION");
     }
 
