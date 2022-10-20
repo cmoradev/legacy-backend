9129da5a00705131a93bf08d5e1efa4d5b5121f0
Francisco Delgado <2005040@utrivieramaya.edu.mx>
2022-10-20 14:43:13 -0500

    feat se creo el reporte de pagos facturados de colegio, tambien para que se genere el excel de pagos facturados

--- src/school-colegio-ingles/charges-school/school-charges-payments/reports/school-payment-invoice.excel.ts
+++ src/school-colegio-ingles/charges-school/school-charges-payments/reports/school-payment-invoice.excel.ts
@@ -7,7 +7,7 @@ import {getNameReport} from '../../../../mini-store/store-sales/mini-store-sales
 
 const esMx = require('moment/locale/es-mx');
 
-export class SchoolPaymentExcel {
+export class SchoolPaymentInvoiceExcel {
     private rows: NotInvoiced[] = [];
     private params: IQueryReportSchoolPayment;
     private workbook: Workbook;
@@ -21,7 +21,7 @@ export class SchoolPaymentExcel {
         this.config();
         this.generate(
             this.addWorksheet(
-                `${getNameReport('Pagos', this.params).excel}`,
+                `${getNameReport('Pagos_Facturados', this.params).excel}`,
             ),
         );
     }
@@ -52,28 +52,29 @@ export class SchoolPaymentExcel {
     private generate(worksheet: Worksheet): Worksheet {
         let columns: TableColumnProperties[] = []
 
-            columns = [
-                { name: 'Matricula', filterButton: true },
-                { name: 'Nombre', filterButton: false },
-                { name: 'Fecha de creación', filterButton: true },
-                { name: 'Folio de venta', filterButton: false },
-                { name: 'Folio de pago', filterButton: false },
-                { name: 'Folio de factura', filterButton: false },
-                { name: 'Ciclo de venta', filterButton: false },
-                { name: 'Metodo de pago', filterButton: true },
-                {
-                    name: 'Total del pago',
-                    filterButton: false,
-                    totalsRowLabel: 'Total',
-                    totalsRowFunction: 'sum',
-                },
-                { name: 'Realizado por', filterButton: false },
-                { name: 'Cancelado por', filterButton: false },
-            ];
+        columns = [
+            { name: 'Folio de venta', filterButton: false },
+            { name: 'Folio de pago', filterButton: false },
+            { name: 'Clave', filterButton: true },
+            { name: 'Nombre', filterButton: false },
+            { name: 'Fecha de pago', filterButton: true },
+            { name: 'Folio de factura', filterButton: false },
+            { name: 'RFC', filterButton: false },
+            { name: 'Fecha de facturación', filterButton: true },
+            { name: 'Identificador único de factura', filterButton: false },
+            { name: 'Identificador global de pago', filterButton: false },
+            { name: 'Metodo de pago', filterButton: true },
+            {
+                name: 'Total del pago',
+                filterButton: false,
+                totalsRowLabel: 'Total',
+                totalsRowFunction: 'sum',
+            },
+        ];
 
         worksheet.mergeCells(`B2:K2`);
         const title = worksheet.getCell('B2');
-        title.value = `Reporte de ${getNameReport('Pagos', this.params).title}`
+        title.value = `Reporte de ${getNameReport('Pagos_Facturados', this.params).title}`
         title.style = {
             alignment: { horizontal: 'center', vertical: 'middle' },
         };
@@ -97,17 +98,18 @@ export class SchoolPaymentExcel {
         const rows = [];
         this.rows.forEach((value: NotInvoiced) => {
             const columns = [];
+            columns.push(value.v_folio);
+            columns.push(value.p_folio);
             columns.push(value.a_key);
             columns.push(value.a_fullname);
             columns.push(formatDate(value.p_created_at));
-            columns.push(value.v_folio);
-            columns.push(value.p_folio);
             columns.push(value.f_folio);
-            columns.push(value.v_cycle);
+            columns.push(value.f_rfc);
+            columns.push(formatDate(value.f_created_at));
+            columns.push(value.f_uuid);
+            columns.push(value.p_global_uuid);
             columns.push(value.f_metodo_pago);
             columns.push(parseFloat(`${value.p_income}`));
-            columns.push(value.u_fullname_cashier);
-            columns.push(value.us_fullname_cancelation);
             rows.push(columns);
         });
         worksheet.addTable({
