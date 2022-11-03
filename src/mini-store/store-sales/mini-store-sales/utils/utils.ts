f3ac35bbe68abfb9cd7e90ed8f977b1bae9d7f64
rafaelpech <rafa.arps@gmail.com>
2022-11-03 09:29:51 -0600

    feat: ventas al dia por cliente

---
+++ src/mini-store/store-sales/mini-store-sales/utils/utils.ts
@@ -0,0 +1,20 @@
+import {IReportSaleTodayRow} from "../types/IReport";
+import {Decimal} from '@munyaal/calculations';
+
+export const reportSaleTodayByClient = (
+    data: IReportSaleTodayRow[]
+) => {
+    const dataClient: IReportSaleTodayRow[] = [];
+    data.forEach((d: any) => {
+        const index = dataClient.findIndex((dd) => d.studentId == dd.studentId);
+        if (index > -1) {
+            dataClient[index].idsPagos.push(...d.idsPagos)
+            dataClient[index].idsDetalles.push(...d.idsDetalles)
+            dataClient[index].TotalDetalles = Decimal.sum(dataClient[index].TotalDetalles, d.TotalDetalles).toNumber()
+            dataClient[index].countSale = Decimal.sum(dataClient[index].countSale, 1).toNumber();
+        } else {
+            dataClient.push({...d, countSale: 1});
+        }
+    })
+    return dataClient;
+}
