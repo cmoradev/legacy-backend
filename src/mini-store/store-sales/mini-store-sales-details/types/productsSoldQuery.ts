65ba66137594e92a22f8dd771b87769fd0978725
Isai Chi Lanestoza <90685460+IsaiCL@users.noreply.github.com>
2022-11-05 13:28:16 -0500

    FIX - Corrección del archivo de tipado productsSoldQuery.ts

--- src/mini-store/store-sales/mini-store-sales-details/types/productsSoldQuery.ts
+++ src/mini-store/store-sales/mini-store-sales-details/types/productsSoldQuery.ts
@@ -1,18 +1,25 @@
-export interface QueryReportProductsSold {
-  start_date?: Date,
-  isExported?: boolean
+import { TypeInformativeReport } from '../../../../common/enums/typeInformativeReport.enum';
+import { IQueryReportSaleToday } from '../../mini-store-sales/types/IReport';
+
+
+export interface QueryReportProductsSold extends IQueryReportSaleToday {
+  type?: TypeInformativeReport
 }
 
 export interface ReportProductsSoldRow {
-  salesDetailsId: number,
-  vd_start_date: Date,
-  vd_measurement_unit: string,
-  vd_quantity: number,
-  productsId: number,
-  product_name: string,
-  product_price: number,
-  product_price_IVA: number,
-  product_IVA: boolean,
-  classificationsId: number,
-  classifications_name: string
+  salesDetailsId: number;
+  vd_createdAt: Date;
+  vd_measurement_unit: string;
+  vd_quantity: number;
+  productsId: number;
+  product_name: string;
+  product_price: number;
+  product_price_IVA: number;
+  product_IVA: boolean;
+  planteles_id: number,
+  planteles_name: string,
+  ciclo_id: number,
+  ciclo_name: string,
+  classificationsId: number;
+  classifications_name: string;
 }
