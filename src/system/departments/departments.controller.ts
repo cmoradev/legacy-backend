7f95ce63f5d373d28cc731bea799235334681116
Francisco Delgado <2005040@utrivieramaya.edu.mx>
2022-11-11 10:30:18 -0500

    feat - se añadio el softDelete y softRestore en departments

--- src/system/departments/departments.controller.ts
+++ src/system/departments/departments.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Delete, Param, ParseIntPipe, Put} from '@nestjs/common';
 import { Crud, CrudController } from '@nestjsx/crud';
 import { Department } from './entities/department.entity';
 import { DepartmentsService } from './departments.service';
@@ -8,6 +8,11 @@ import { DepartmentsService } from './departments.service';
         type: Department,
     },
     query: {
+        filter: {
+            deletedAt: {
+                $eq: null,
+            },
+        },
         limit: 10,
         join: {},
     },
@@ -19,5 +24,15 @@ export class DepartmentsController implements CrudController<Department> {
     }
     get base(): CrudController<Department> {
         return this;
+    }
+
+    @Delete('soft-deleted/:id')
+    public async softDeleteOne(@Param('id', ParseIntPipe) id: number) {
+        return await this.service.softDeleteOne(id);
+    }
+
+    @Put('soft-restore/:id')
+    public async softRestoreOne(@Param('id', ParseIntPipe) id: number) {
+        return await this.service.softRestoreOne(id);
     }
 }
