4bde1f4c5bcd1f3f109546ace74c29a2d9776839
Francisco Delgado <2005040@utrivieramaya.edu.mx>
2022-11-11 09:58:55 -0500

    feat - se añadio el softDelete y softRestore en school-charges-invoice

--- src/school-colegio-ingles/charges-school/school-charges-invoice/school-charges-invoice.service.ts
+++ src/school-colegio-ingles/charges-school/school-charges-invoice/school-charges-invoice.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -10,7 +10,6 @@ import * as nodemailer from 'nodemailer';
 import Mail from 'nodemailer/lib/mailer';
 import { InvoiceProcessorCollege } from './utils/invoice.processor';
 import { BranchOfficeSettingService } from '../../../system/branch-office-setting/branch-office-setting.service';
-import { ReportInvoice } from '../../../mini-store/store-sales/mini-store-invoices/reports/invoice.report';
 import { ConfigService } from '../../../common/config/config.service';
 import * as moment from 'moment';
 
@@ -23,6 +22,22 @@ export class SchoolChargesInvoiceService extends TypeOrmCrudService<SchoolCharge
     private readonly configService: ConfigService,
   ) {
     super(repo);
+  }
+
+  public async softDeleteOne(id: number) {
+    const object = await this.findOne(id);
+    if (!object) {
+      throw new NotFoundException('This entity does not exists')
+    }
+    return await this.repo.softDelete(id);
+  }
+
+  public async softRestoreOne(id: number) {
+    const object = await this.repo.findOne({id}, {withDeleted: true});
+    if (!object) {
+      throw new NotFoundException('This entity does not exists')
+    }
+    return await this.repo.restore(id);
   }
 
   async findInvoiceByPayment(options: { paymentId: number, status: StatusInvoce, stamping?: number }) {
