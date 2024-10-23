69d5405cca18713785d4095aa15c632a9b21b31a
Caleb Isaac Mora Diaz <calexico659@gmail.com>
2024-10-23 11:45:13 -0500

    fix: files imports

--- src/school-colegio-ingles/school-reports/dto/index.ts
+++ src/school-colegio-ingles/school-reports/dto/index.ts
@@ -2,5 +2,5 @@ export * from './school-income-query';
 export * from './school-invoice-query';
 export * from './school-income-group-query';
 export * from './school-group-query';
-export * from './School-debit-query';
+export * from './school-debit-query';
 export * from './school-bank-statement-query';
